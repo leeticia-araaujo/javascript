56d0a100dcf4d6d1a9177dd1895a08f87c0e8ad4
Letícia Cristina <128254784+leeticia-araaujo@users.noreply.github.com>
2024-06-16 23:00:35 -0300

    Add files via upload

---
+++ atividades/01-primeiro-script/teste.js
@@ -0,0 +1,22 @@
+console.log("teste");
+
+var n =1;
+console.log(`n ${n}`);
+
+n=n+12;
+console.log(`n ${n}`);
+
+n=n-7;
+console.log(`n ${n}`);
+
+n=n/2;
+console.log(`n ${n}`);
+
+n=n*3;
+console.log(`n ${n}`);
+
+n=n%2;
+console.log(`n ${n}`);
+
+n=n**3;
+console.log(`n ${n}`);
